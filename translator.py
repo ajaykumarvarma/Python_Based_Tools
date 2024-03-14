31e141cc180a333fae348e09d70b231c8c6c6511
ajaykumarvarma32 <ajaynagaraju32@gmail.com>
2024-03-14 18:21:51 +0530

    newly updated

---
+++ translator.py
@@ -0,0 +1,33 @@
+from deep_translator import GoogleTranslator
+
+#source -> your language
+#target -> Target language for example if it is spanish then use -> es
+
+translator = GoogleTranslator(
+    source='auto',
+    target='es'
+)
+
+text = "_put your text here_"
+
+print(translator.translate(text))
+
+
+#----------------------------------
+
+# from textblob import TextBlob
+
+# text = "Hello, world!"
+# blob = TextBlob(text)
+# translation = blob.translate(to='es')
+# print(translation)
+
+#----------------------------------
+
+# from pytranslate import translate
+
+# text = "Hello, world!"
+# translation = translate(text, target_lang='es')
+# print(translation)
+
+#----------------------------------
